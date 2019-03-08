dca1e96c55ae1779366a6263f5659719deb0ddfc
SteveJacksonSoft <stephen.jackson@softwire.com>
2019-03-08 11:35:50 +0000

    Red-3 tests (pawns cannae move if there is something in their way) passing.

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -5,7 +5,9 @@ import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Pawn extends AbstractPiece {
@@ -16,17 +18,22 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
-        if (this.colour == PlayerColour.WHITE) {
-            allowedMoves.add(new Move(from, from.plus(-1, 0)));
-            if (from.getRow() == 6) {
-                allowedMoves.add(new Move(from, from.plus(-2, 0)));
-            }
-        } else if (this.colour == PlayerColour.BLACK){
-            allowedMoves.add(new Move(from, from.plus(1, 0)));
-            if (from.getRow() == 1) {
-                allowedMoves.add(new Move(from, from.plus(2, 0)));
-            }
+        Coordinates[] squaresInFront = this.squaresInFront(from);
+        if (board.get(squaresInFront[0]) == null) {
+            allowedMoves.add(new Move(from, squaresInFront[0]));
+        }
+        if ((this.colour == PlayerColour.WHITE && from.getRow() == 6)
+                || (this.colour == PlayerColour.BLACK && from.getRow() == 1)) {
+            allowedMoves.add(new Move(from, squaresInFront[1]));
         }
         return allowedMoves;
+    }
+
+    private Coordinates[] squaresInFront(Coordinates currentSquare) {
+        if (this.colour == PlayerColour.BLACK) {
+            return new Coordinates[]{currentSquare.plus(1, 0), currentSquare.plus(2, 0)};
+        } else {
+            return new Coordinates[]{currentSquare.plus(-1, 0), currentSquare.plus(-2, 0)};
+        }
     }
 }
