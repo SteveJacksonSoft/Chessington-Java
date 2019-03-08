8a810d419fe0b0a6ba7181cd83c766cc44c68587
SteveJacksonSoft <stephen.jackson@softwire.com>
2019-03-08 12:30:34 +0000

    Red-5 tests (pawns can capture diagonally) passing.

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -5,10 +5,9 @@ import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Pawn extends AbstractPiece {
     public Pawn(PlayerColour colour) {
@@ -18,24 +17,70 @@ public class Pawn extends AbstractPiece {
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
         List<Move> allowedMoves = new ArrayList<>();
-        Coordinates[] squaresInFront = this.squaresInFront(from);
-        if (board.contains(squaresInFront[0])) {
-            if (board.get(squaresInFront[0]) == null) {
-                allowedMoves.add(new Move(from, squaresInFront[0]));
-            }
-        }
-        if ((this.colour == PlayerColour.WHITE && from.getRow() == 6)
-                || (this.colour == PlayerColour.BLACK && from.getRow() == 1)) {
-            allowedMoves.add(new Move(from, squaresInFront[1]));
-        }
+
+        Optional.ofNullable(this.getStandardMove(from, board))
+                .ifPresent(allowedMoves::add);
+        allowedMoves.addAll(this.getAttackingMoves(from, board));
+        Optional.ofNullable(this.getDoubleMove(from))
+                .ifPresent(allowedMoves::add);
         return allowedMoves;
     }
 
-    private Coordinates[] squaresInFront(Coordinates currentSquare) {
+    private Coordinates nextSquareForward(Coordinates currentSquare) {
         if (this.colour == PlayerColour.BLACK) {
-            return new Coordinates[]{currentSquare.plus(1, 0), currentSquare.plus(2, 0)};
+            return currentSquare.plus(1, 0);
         } else {
-            return new Coordinates[]{currentSquare.plus(-1, 0), currentSquare.plus(-2, 0)};
+            return currentSquare.plus(-1, 0);
         }
     }
+
+    private List<Move> getAttackingMoves(Coordinates currentSquare, Board board) {
+        List<Move> allowedMoves = new ArrayList<>();
+        Coordinates squareInFront = this.nextSquareForward(currentSquare);
+        try {
+            Optional.ofNullable(board.get(squareInFront.plus(0,1)))
+                    .ifPresent(piece -> {
+                        if (piece.getColour() == PlayerColour.WHITE && this.colour == PlayerColour.BLACK) {
+                            allowedMoves.add(new Move(currentSquare, squareInFront.plus(0, 1)));
+                        }
+                        if (piece.getColour() == PlayerColour.BLACK && this.colour == PlayerColour.WHITE) {
+                            allowedMoves.add(new Move(currentSquare, squareInFront.plus(0, 1)));
+                        }
+                    });
+            Optional.ofNullable(board.get(squareInFront.plus(0,-1)))
+                    .ifPresent(piece -> {
+                        if (piece.getColour() == PlayerColour.WHITE && this.colour == PlayerColour.BLACK) {
+                            allowedMoves.add(new Move(currentSquare, squareInFront.plus(0, -1)));
+                        }
+                        if (piece.getColour() == PlayerColour.BLACK && this.colour == PlayerColour.WHITE) {
+                            allowedMoves.add(new Move(currentSquare, squareInFront.plus(0, -1)));
+                        }
+                    });
+        } catch (IndexOutOfBoundsException e) {}
+        return allowedMoves;
+    }
+
+    private Move getStandardMove(Coordinates currentSquare, Board board) {
+        try {
+            Coordinates squareInFront = this.nextSquareForward(currentSquare);
+            if (board.get(squareInFront) == null) {
+                return new Move(currentSquare, squareInFront);
+            } else {
+                return null;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    private Move getDoubleMove(Coordinates currentSquare) {
+        if (this.colour == PlayerColour.WHITE && currentSquare.getRow() == 6) {
+            return new Move(currentSquare, currentSquare.plus(-2, 0));
+        } else if (this.colour == PlayerColour.BLACK && currentSquare.getRow() == 1) {
+            return new Move(currentSquare, currentSquare.plus(2, 0));
+        } else {
+            return null;
+        }
+    }
+
 }
